7e002b5113cfbfd2a6fdd20f97e6c13e2173e429
Listyo <Email>
2026-07-02 10:44:07 +0700

    Buat model & migration

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    protected $fillable = ['product_id', 'path', 'is_primary', 'order'];
+
+    protected function casts(): array
+    {
+        return [
+            'is_primary' => 'boolean',
+            'order' => 'integer',
+        ];
+    }
 }
