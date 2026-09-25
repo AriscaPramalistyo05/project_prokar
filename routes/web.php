676b1daada50aa4672ec5aec03472e445847d4cb
Listyo <Email>
2026-09-25 19:42:39 +0700

    fix(routes): remove duplicate DocImageController and DocController imports in web.php

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\Auth\GoogleController;
 use App\Http\Controllers\Frontend\CheckoutController;
 use App\Http\Controllers\Frontend\DocController;
 use App\Http\Controllers\Frontend\ProductController;
-use App\Http\Controllers\Admin\DocImageController;
-use App\Http\Controllers\Frontend\DocController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderInvoiceController;
 use App\Http\Controllers\VideoStreamController;
